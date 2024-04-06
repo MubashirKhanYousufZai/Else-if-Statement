007bad3e102935eee977bd5f0a376ef8bfe51a2f
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-06 16:39:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+// Else-if Statement
+let isWorking = "in office day";
+if (isWorking === "on sunday") {
+    console.log("Might busy.");
+}
+else if (isWorking === "in Office days.") {
+    console.log("regular working hours.");
+}
+else {
+    console.log("Urgent deadlines.");
+}
+let areStuding = "together";
+if (areStuding === "alone") {
+    console.log("Set clear goal.");
+}
+else if (areStuding === "togther") {
+    console.log("exams are starting soon.");
+}
+else {
+    console.log("Collaborating on assignments.");
+}
+let isthirsty = "need water.";
+if (isthirsty === "need water.") {
+    console.log("Grab a glass of water.");
+}
+else if (isthirsty === "need a juice.") {
+    console.log("hydrating option.");
+}
+else {
+    console.log("enjoying tea.");
+}
